9a1228ecc40a800ac9b65fe9008a6a36c81d56dd
Nguyễn Giang Nam <namnghs150081@fpt.edu.vn>
2023-03-24 22:49:37 +0700

    update products.cshtml

--- eStore/Controllers/ProductController.cs
+++ eStore/Controllers/ProductController.cs
@@ -6,14 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Drawing;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace eStore.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Products([FromQuery] PaginationParams @params, string? search, int? categoryId)
+        private static readonly string BaseUrl = "https://localhost:7177/";
+
+        public IActionResult Products([FromQuery] PaginationParams @params, string? search, int? categoryId, int item)
         {
-            if (@params.ItemsPerPage == 0) @params.ItemsPerPage = 8;
+            if (@params.ItemsPerPage == 0) @params.ItemsPerPage = 10;
+            if (item > 10) @params.ItemsPerPage = item;
 
             var conn = string.IsNullOrEmpty(search)
                 ? $"api/Products?Page={@params.Page}&ItemsPerPage={@params.ItemsPerPage}"
@@ -22,8 +27,8 @@ namespace eStore.Controllers
                ? $"{conn}"
                : $"{conn}&categoryId={categoryId}";
             var _conn = $"api/Categories/selectlist";
-            var Res = ResponseConfig.GetData(conn).Result;
-            var _Res = ResponseConfig.GetData(_conn).Result;
+            var Res = GetData(conn).Result;
+            var _Res = GetData(_conn).Result;
             var products = JsonConvert.DeserializeObject<List<ProductRes>>(Res.Content.ReadAsStringAsync().Result);
             var pagination = JsonConvert.DeserializeObject<PaginationMetadata>(Res.Headers.GetValues("X-Pagination").FirstOrDefault()!);
             List<CateSelectRes>? category = JsonConvert.DeserializeObject<List<CateSelectRes>>(_Res.Content.ReadAsStringAsync().Result);
@@ -31,6 +36,7 @@ namespace eStore.Controllers
             ViewData["pagination"] = pagination;
             ViewBag.categories = category;
             ViewBag.categoryId = categoryId;
+            ViewBag.Item = item;
             return View(products);
         }
 
@@ -124,7 +130,78 @@ namespace eStore.Controllers
             return RedirectToAction("Products");
         }
 
+        public async Task<HttpResponseMessage> GetData(string targetAddress)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (!string.IsNullOrEmpty(HttpContext.Request.Cookies["accessToken"]))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Request.Cookies["accessToken"]);
+            }
+            var Response = await client.GetAsync(targetAddress);
+            return Response;
+        }
 
+        public async Task<HttpResponseMessage> PostData(string targetAddress, string content)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (!string.IsNullOrEmpty(HttpContext.Request.Cookies["accessToken"]))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Request.Cookies["accessToken"]);
+            }
+            var Response = await client.PostAsync(targetAddress, new StringContent(content, Encoding.UTF8, "application/json"));
+            return Response;
+        }
+
+        public async Task<HttpResponseMessage> UploadData(string targerAddress, byte[] bytes, string fileName)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var multiPartFromDataContent = new MultipartFormDataContent();
+            var fileContent = new ByteArrayContent(bytes);
+            multiPartFromDataContent.Add(fileContent, "file", fileName);
+            if (!string.IsNullOrEmpty(HttpContext.Request.Cookies["accessToken"]))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Request.Cookies["accessToken"]);
+            }
+            var Response = await client.PostAsync(targerAddress, multiPartFromDataContent);
+            return Response;
+        }
+
+        public async Task<HttpResponseMessage> PutData(string targetAddress, string content)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (!string.IsNullOrEmpty(HttpContext.Request.Cookies["accessToken"]))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Request.Cookies["accessToken"]);
+            }
+            var Response = await client.PutAsync(targetAddress, new StringContent(content, Encoding.UTF8, "application/json"));
+            return Response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteData(string targetAddress, string content)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (!string.IsNullOrEmpty(HttpContext.Request.Cookies["accessToken"]))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Request.Cookies["accessToken"]);
+            }
+            var Response = await client.DeleteAsync(targetAddress);
+            return Response;
+        }
 
     }
 }
